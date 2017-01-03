4a2ef46ea5ef0b86a9037744226f341df765f4b3
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-03 15:55:37 -0800

    adding counter

--- game.py
+++ game.py
@@ -5,22 +5,26 @@ print "Hi Friend!"
 
 player = raw_input("What is your name? ")
 
-ran_num = random.randint(1,100)
+ran_num = random.randint(1, 100)
 
 print ran_num
 
 print "Hi, " + player + ". I am thinking of a number between 1 and 100."
-print "Try to guess my number."
+print "Try to guess my number. "
 
 
-guess = int(raw_input("What number do you guess?"))
+guess = int(raw_input("What number do you guess? "))
+i = 1
+
 
 while (guess != ran_num):
     if (guess > ran_num):
         print "Your number is too big!"
-        guess = int(raw_input("What number do you guess?"))
+        guess = int(raw_input("What number do you guess? "))
+        i = i + 1
     elif (guess < ran_num):
         print "Your number is too small!"
-        guess = int(raw_input("What number do you guess?"))
+        guess = int(raw_input("What number do you guess? "))
+        i = i + 1
 
-print "You got it"
+print "You got it in " + str(i) + " attemps"
