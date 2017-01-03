048e2fc52b0abbe18b99bfc80fbbc078ca9851f5
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-03 15:46:04 -0800

    checking loop

--- game.py
+++ game.py
@@ -12,7 +12,15 @@ print ran_num
 print "Hi, " + player + ". I am thinking of a number between 1 and 100."
 print "Try to guess my number."
 
-guess = raw_input("What number do you guess?")
 
-if guess > ran_num:
-    print "Your number is too big!"
+guess = int(raw_input("What number do you guess?"))
+
+while (guess != ran_num):
+    if (guess > ran_num):
+        print "Your number is too big!"
+        guess = int(raw_input("What number do you guess?"))
+    elif (guess < ran_num):
+        print "Your number is too small!"
+        guess = int(raw_input("What number do you guess?"))
+
+print "You got it"
