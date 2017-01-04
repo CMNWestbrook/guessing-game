a702ff4b2f63b3e7a36c489e65e0535a91c3b42a
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-03 16:19:50 -0800

    cleaner code

--- game.py
+++ game.py
@@ -9,22 +9,22 @@ ran_num = random.randint(1, 100)
 
 print ran_num
 
-print "Hi, " + player + ". I am thinking of a number between 1 and 100."
+print "Hi, %s. I am thinking of a number between 1 and 100." % player
 print "Try to guess my number. "
 
 
 guess = int(raw_input("What number do you guess? "))
-i = 1
+tries = 1
 
+while guess != ran_num:
+    if guess > ran_num:
+        side = "big"
+    elif guess < ran_num:
+        side = "small"
 
-while (guess != ran_num):
-    if (guess > ran_num):
-        print "Your number is too big!"
-        guess = int(raw_input("What number do you guess? "))
-        i = i + 1
-    elif (guess < ran_num):
-        print "Your number is too small!"
-        guess = int(raw_input("What number do you guess? "))
-        i = i + 1
+    print "Your number is too %s!" % side
+    guess = int(raw_input("What number do you guess? "))
+    tries = tries + 1
 
-print "You got it in " + str(i) + " attemps"
+print "You got it in %d attemps" % tries
+
